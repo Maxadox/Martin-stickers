8ba9b1b66bffcefa0592fbd90a4b089bd2064307
Maxadox <116370591+Maxadox@users.noreply.github.com>
2026-06-14 21:50:25 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.SYNC_CONFIG = {
+  apiKey: "AIzaSyCirCsbPwYgzxZf8MODFSWPbKACzeQhGP8",
+  authDomain: "martin-stickers.firebaseapp.com",
+  databaseURL: "https://martin-stickers-default-rtdb.firebaseio.com",
+  projectId: "martin-stickers",
+  storageBucket: "martin-stickers.firebasestorage.app",
+  messagingSenderId: "132789290937",
+  appId: "1:132789290937:web:c790594526031c5b171868"
+};
